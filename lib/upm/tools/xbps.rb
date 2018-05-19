455fb4b37097fdc47dcd826142b5a773a3302842
epitron <chris@ill-logic.com>
2018-05-19 04:04:34 -0400

    'xbps' cleanup

--- lib/upm/tools/xbps.rb
+++ lib/upm/tools/xbps.rb
@@ -4,18 +4,19 @@ UPM::Tool.new "xbps" do
 
   identifying_binary "xbps-install"
 
-  command "install", "xbps-install", root: true
-  command "update",  "xbps-install -S", root: true
-  command "upgrade", "xbps-install -Su", root: true
-  command "files",   "xbps-query -f"
-  command "search",  "xbps-query --regex -Rs"
-  # command "info", ""
+  command "install", "xbps-install",      root: true
+  command "update",  "xbps-install -S",   root: true
+  command "upgrade", "xbps-install -Su",  root: true
+
+  command "files",   "xbps-query -f",           paged: true
+  command "search",  "xbps-query --regex -Rs",  paged: true
   
   command "list" do |args|
     if args.any?
-      run "xbps-query", "-f", *args
+      query = args.join
+      run "xbps-query", "-l", grep: query, paged: true
     else
-      run "xbps-query", "-l"
+      run "xbps-query", "-l", paged: true
     end
   end
 
